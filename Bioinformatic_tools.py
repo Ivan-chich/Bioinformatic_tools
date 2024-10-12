aa59c987130975cc475c10138cae9b10b6de40ae
Ivan Chicherin <i.v.chicherin@gmail.com>
2024-10-12 23:04:52 +0300

    changed filter_fastq, added bio_files_processor

--- Bioinformatic_tools.py
+++ Bioinformatic_tools.py
@@ -1,13 +1,14 @@
 import aux_mod.dna_rna_aux_mod as dnarna
 import aux_mod.filter_fastq_aux_mod as filter
-from typing import Union
+import os
+from typing import Union, TextIO
 
 instruments = {
-    'transcribe': dnarna.transcribe,
-    'reverse': dnarna.reverse,
-    'complement': dnarna.complement,
-    'reverse_complement': dnarna.reverse_complement
-    }
+    "transcribe": dnarna.transcribe,
+    "reverse": dnarna.reverse,
+    "complement": dnarna.complement,
+    "reverse_complement": dnarna.reverse_complement,
+}
 
 
 def run_dna_rna_tools(*args: str) -> Union[str, list]:
@@ -24,9 +25,9 @@ def run_dna_rna_tools(*args: str) -> Union[str, list]:
     data = args[:-1]
     tool = instruments[args[-1]]
     for sequence in data:
-        if dnarna.is_dna(sequence) is False and dnarna.is_rna(
-                                      sequence) is False:
-            result.append('This is not DNA or RNA sequence!')
+        if dnarna.is_dna(sequence
+                         ) is False and dnarna.is_rna(sequence) is False:
+            result.append("This is not DNA or RNA sequence!")
         else:
             result.append(tool(sequence))
     if len(result) == 1:
@@ -35,9 +36,13 @@ def run_dna_rna_tools(*args: str) -> Union[str, list]:
         return result
 
 
-def filter_fastq(seqs: dict, gc_bounds: Union[tuple, float] = (0, 100),
-                 length_bounds: Union[tuple, int] = (0, 2 ** 32),
-                 quality_threshold: int = 0) -> dict:
+def filter_fastq(
+    input_fastq: TextIO,
+    output_fastq: Union[str, os.PathLike],
+    gc_bounds: Union[tuple, float] = (0, 100),
+    length_bounds: Union[tuple, int] = (0, 2**32),
+    quality_threshold: int = 0,
+) -> None:
     """Function filter_fastq
 
     Take a dictionary with FASTQ sequences and
@@ -51,25 +56,20 @@ def filter_fastq(seqs: dict, gc_bounds: Union[tuple, float] = (0, 100),
     Returns: dict
     """
     filtered_seqs = dict()
-
     if type(gc_bounds) is not tuple:
         gc_bounds = (0, gc_bounds)
     if type(length_bounds) is not tuple:
         length_bounds = (0, length_bounds)
-
+    seqs = filter.fastq_to_dict(input_fastq)
     for name in seqs:
-
         sequence, quality = seqs[name]
-
-        len_flag = len(sequence) >= length_bounds[0] and len(
-            sequence) <= length_bounds[1]
-        gc_flag = filter.gc_content(
-            sequence) >= gc_bounds[0] and filter.gc_content(
-            sequence) <= gc_bounds[1]
-        qual_flag = filter.seq_quality(
-            quality) >= quality_threshold
-
-        if len_flag and gc_flag and qual_flag:
+        if (
+            len(sequence) >= length_bounds[0]
+            and len(sequence) <= length_bounds[1]
+            and filter.gc_content(sequence) >= gc_bounds[0]
+            and filter.gc_content(sequence) <= gc_bounds[1]
+            and filter.seq_quality(quality) >= quality_threshold
+        ):
             filtered_seqs[name] = (sequence, quality)
-
-    return filtered_seqs
+    filter.write_in_file(filtered_seqs, output_fastq)
+    return
--- aux_mod/dna_rna_aux_mod.py
+++ aux_mod/dna_rna_aux_mod.py
@@ -1,13 +1,35 @@
+dct_dna = {
+    "A": "T",
+    "T": "A",
+    "G": "C",
+    "C": "G",
+    "a": "t",
+    "t": "a",
+    "g": "c",
+    "c": "g",
+}
+dct_rna = {
+    "A": "U",
+    "U": "A",
+    "G": "C",
+    "C": "G",
+    "a": "u",
+    "u": "a",
+    "g": "c",
+    "c": "g",
+}
+
+
 def is_dna(seq: str) -> str:
-    return set(seq.upper()).issubset({'A', 'T', 'G', 'C'})
+    return set(seq.upper()).issubset({"A", "T", "G", "C"})
 
 
 def is_rna(seq: str) -> str:
-    return set(seq.upper()).issubset({'A', 'U', 'G', 'C'})
+    return set(seq.upper()).issubset({"A", "U", "G", "C"})
 
 
 def transcribe(seq: str) -> str:
-    for i in (('T', 'U'), ('t', 'u')):
+    for i in (("T", "U"), ("t", "u")):
         seq = seq.replace(*i)
     return seq
 
@@ -17,16 +39,12 @@ def reverse(seq: str) -> str:
 
 
 def complement(seq: str) -> str:
-    dct_dna = {'A': 'T', 'T': 'A', 'G': 'C', 'C': 'G',
-               'a': 't', 't': 'a', 'g': 'c', 'c': 'g'}
-    dct_rna = {'A': 'U', 'U': 'A', 'G': 'C', 'C': 'G',
-               'a': 'u', 'u': 'a', 'g': 'c', 'c': 'g'}
     bases = list(seq)
     if is_dna(seq) is True:
         bases = [dct_dna[letter] for letter in bases]
     if is_rna(seq) is True:
         bases = [dct_rna[letter] for letter in bases]
-    return ''.join(bases)
+    return "".join(bases)
 
 
 def reverse_complement(seq: str) -> str:
